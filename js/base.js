cdf11c1283c267b028282ee5a1b26d7be502da1c
冯略 <fenglue@netwaymedia.com>
2018-05-08 14:24:58 +0800

    test

--- js/base.js
+++ js/base.js
@@ -2,6 +2,74 @@
  * @method Tab切换
  * @param {String} 参数一：nav栏标签；参数二：content栏标签;参数三：激活样式
  */
+$(".item-content-item").eq(0).css("display","block")
+function tabSwitch(navEle, contentEle, activeClass, callBack) {
+ 
+  $(navEle).on("click", function () {
+    
+    $(this)
+      .siblings()
+      .removeClass(activeClass);
+    $(this).addClass(activeClass);
+    $(contentEle).hide();
+    $(contentEle)
+      .eq($(this).find("span").attr("data-ind"))
+      .show();
+ 
+    if (callBack) {
+      callBack();
+    }
+  });
+}
+
+// 点击右侧下拉箭头，出现选项，选中改变tabs列表以及内容；
+testTab()
+
+function testTab() {
+  $("body").on("click", ".item-nav>i", function (event) {
+    event.stopPropagation();
+    if (!$(this).hasClass("on")) {
+      $(this).addClass("on");
+      $(".testTab").show()
+    } else {
+      $(this).removeClass("on");
+      $(".testTab").hide()
+    }
+  })
+  var firstNum,
+    lastNum
+  $("body").on("click", ".testTab .testTab-item", function (event) {
+    event.stopPropagation();
+    firstNum = $.trim($(this).find(".firstNum").text());
+    lastNum =  $.trim($(this).find(".lastNum").text());
+    // 每次切换第一个显示
+    var a = $(".task-header select").val();
+    $(".item-content-item").hide();
+    $(".homework-item").eq(a).find(".item-content-item").eq(firstNum-1).show();
+    $(".item-nav .item-nav-item").removeClass("active");
+    $(".homework-item").eq(a).find(".item-nav .item-nav-item").eq(0).addClass("active");
+    for (i = firstNum; i <= lastNum;) {
+      for (j = 0; j < 10; j++) {
+        
+        $(".homework-item").eq(a).find(".item-nav .item-nav-item").eq(j).find("h3 span").text(i).attr("data-ind", (i - 1))
+        i++;
+      }
+    }
+    $(".item-nav>i").removeClass("on");
+    $(".testTab").hide();
+    
+  })
+
+}
+// 阶段选择
+$(".task-header select").bind("change",function(){
+  var a = $(".task-header select").val();
+  $(".homework-item").hide();
+  $(".homework-item").eq(a).show();
+  $(".homework-item").eq(a).find(".item-content-item").eq(0).show();
+  $(".homework-item").eq(a).find(".item-nav-item").removeClass("active")
+  $(".homework-item").eq(a).find(".item-nav-item").eq(0).addClass("active");
+})
 
 /**
  * 
@@ -18,7 +86,7 @@ function timeCountDown(time, callBack) {
       $('.time-count-down .time').html("00:00:00");
       callBack && callBack();
     }
-   
+
     // $('.time-count-down .time').html(currentTime)
   }, 1000)
 }
@@ -39,3 +107,25 @@ function timeFormat(time) {
   var fomart = h + ':' + m + ':' + s;
   return fomart;
 }
+
+$("body").on("click", ".toast-down", function (event) {
+  event.stopPropagation();
+  if (!$(this).hasClass("on")) {
+    $(this).addClass("on");
+    $(this).siblings(".toast").show();
+  } else {
+    $(this).removeClass("on");
+    $(this).siblings(".toast").hide();
+  }
+})
+$("body").on("click", ".toast-item", function () {
+  $(this).parents(".toast").siblings(".toast-down").removeClass("on");
+  $(this).parents(".toast").hide();
+})
+$("body").on("click", function () {
+  $(".toast").hide();
+  $(".toast-down").removeClass("on");
+  $(".testTab").hide();
+  $(".item-nav>i").removeClass("on");
+
+})
---
+++ js/homework_home.js
@@ -0,0 +1,8 @@
+// 作业切换
+
+// tabSwitch(".item-nav .item-nav-item",".item-content .item-content-item","active")
+for(var i = 1; i<11; i++){
+    tabSwitch("#homework-item"+i+" .item-nav .item-nav-item","#homework-item"+i+" .item-content .item-content-item","active")
+}
+// tabSwitch("#homework-item1 .item-nav .item-nav-item","#homework-item1 .item-content .item-content-item","active")
+// tabSwitch("#homework-item2 .item-nav .item-nav-item","#homework-item2 .item-content .item-content-item","active")
